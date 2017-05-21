e1f3278dcda5d61ba74491945dd1ef40b53602c8
rufat24 <rufat.ismaylov94@gmail.com>
2017-05-21 10:49:54 +0400

    The lobby was completed. Now two players can join the room. As soon as two players will be in the room the game will start. Some styles and effects were added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,46 @@
 $(document).ready(function() {
   var h=1;
   var a=[];
+  var socket=io();
+  var color="";
+      $(".map").hide();
+  socket.on('numplayers', function (data) {
+    console.log(data);
+    $('#num').text(data.num);
+    if(parseInt(data.start,10)==1){
+      $(".lobby").hide();
+      $(".map").show();
+    }
+  });
+  socket.on('mycolor', function (data) {
+      color=data.color;
+      console.log(color);
+  });
+
   $('.join').live('click',function() {
+     socket.emit('userjoin',{});
      $('.join').hide();
    });
+
 /*  $('img').mousedown(function(e) {
    $(this).mapster({
      fillOpacity: 0.4,
      fillColor: "d42e16",
    });
 });*/
-
+$('img').mapster({
+  fillOpacity:1,
+  fillColor: color,
+  stroke: true,
+  strokeColor: "000",
+  strokeOpacity: 0.8,
+  strokeWidth: 4
+});
 
 $('area').mouseup(function(e) {
    $(this).mapster('set',false);
-/*   if(h%2==0){
-     $('img').mapster({
-       fillOpacity:1,
-       fillColor: "0000ff",
-       stroke: true,
-       strokeColor: "000",
-       strokeOpacity: 0.8,
-       strokeWidth: 4
-     });}
+
+     /*   if(h%2==0){
 
      else{
        $('img').mapster({
--- server.js
+++ server.js
@@ -2,21 +2,33 @@ var express = require('express');
 var app= express();
 var httpserver=require('http').Server(app);
 var io= require('socket.io')(httpserver);
-
-
+var np=0;
+var maxplayers=2;
+var colors=['0000ff','ff0000'];
 app.use(express.static(__dirname));
 app.set('view engine', 'pug');
-app.get('/', function(req, res){
-  res.render('lobby', {title: "Lobby"});
-});
 
 httpserver.listen(8000, function(){
   console.log("Server listening on port 8000")
 });
-app.listen(8001, function(){
-  console.log("express listening on port 8001")
+app.get('/', function(req, res){
+  res.render('lobby', {title: "Lobby", number:np.toString()});
 });
 
- io.on('connection', function(socket){
-   console.log('a user connected');
+
+
+ io.sockets.on('connection', function(socket){
+   io.emit('numplayers',{num: np.toString(), start: "0"})
+   socket.on('userjoin',function(){
+     np++;
+     socket.emit('mycolor',{color:colors[np-1]});
+     console.log(np.toString());
+     if(np==maxplayers){
+       io.emit('numplayers',{num: np.toString(),start:'1'});
+     }
+     else{
+       io.emit('numplayers',{num: np.toString(),start:'0'});
+     }
+
+   })
  });
